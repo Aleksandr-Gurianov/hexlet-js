fa38526e7cec825a46ab07c25319fc26ffe95a10
Alexander Gyrianov <gyryanovalexander@gmail.com>
2023-05-05 03:05:39 -0300

    new new

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import _ from "lodash";
 
-// Пересечение массивов
-_.intersection([2, 1], [2, 3]); // => [2]
-
-// capitalize делает первую букву заглавной
-_.capitalize("hello"); // Hello
-
-console.log(_.last(['one', 'two']));
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
